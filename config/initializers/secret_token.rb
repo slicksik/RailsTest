3c9ac01d3c79d6d7eac92ca177b6fc366c606f18
Vasileios Topouz <vasileiostopouz@Vasileioss-MacBook-Air.local>
2012-02-06 19:12:27 +0200

    Twitter Bootstrap install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTest::Application.config.secret_token = 'ef7c5bb768986fc262ab12b6ef420ee597ece31cb221d3ebfe797b5338ac3a4f64b5445636b6f2e83d266f80bdff6d3a2c21fc9960ae9cbfafd87afac94f743f'
